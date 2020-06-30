48b52460713ec702a2ed5d687413a133f0dd9c2e
oxford-arc <66992362+oxford-arc@users.noreply.github.com>
2020-06-30 17:17:48 +0100

    Updated results.

--- runbench.sh
+++ runbench.sh
@@ -14,11 +14,21 @@ module load /system/arcus/modules/singularity/3.5.3
 #
 #------------------- END --------------------------
 
+export RESULTS=$PWD/results.txt
+
+rm -f $RESULTS
+touch $RESULTS
+
 for d in gromacs/*
-do 
-	echo $d
-	cd $d
-	singularity run --nv -B ${PWD}:/host_pwd --pwd /host_pwd ../../image/gromacs-2018.2.simg gmx mdrun -deffnm benchmark -ntmpi 1 -ntomp 5
-	cd ../..
+do
+        echo "--------------------------------------------------------" >> $RESULTS
+        echo $d >> $RESULTS
+        cd $d
+        singularity run --nv -B ${PWD}:/host_pwd --pwd /host_pwd ../../image/gromacs-2018.2.simg gmx mdrun -deffnm benchmark -ntmpi 1 -ntomp 5
+        grep -C 3 "CPU info:" benchmark.log >> $RESULTS
+        grep -C 3 "GPU info:" benchmark.log >> $RESULTS
+        grep -C 2 "Performance" benchmark.log >> $RESULTS
+        echo "--------------------------- END ------------------------" >> $RESULTS
+        cd ../..
 done
 
