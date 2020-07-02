7369795a68ee40783a66648bab3f578a7dd27b6b
oxford-arc <66992362+oxford-arc@users.noreply.github.com>
2020-07-02 12:01:39 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ wget http://www.hecbiosim.ac.uk/gromacs-benchmarks/send/2-software/8-gromacs-ben
 mv 8-gromacs-bench hecbiosim-gromacs.tgz
 tar xzf hecbiosim-gromacs.tgz
 
-echo "Building Singularity image from docker file..."
+echo "Downloading Singularity image..."
 cd image
+singularity pull library://oxford-arc/default/gromacs2018.2-test:sha256.a62bf1b6c9a6e850441b13fd74ad70ed24e573c85d1600eb9bd48041cb9eec1d
 sh gromacs.sh
 cd ..
 
