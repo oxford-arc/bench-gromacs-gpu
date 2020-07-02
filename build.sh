d82028d6adbe1ac95299c5bd9eed190eca6bb748
oxford-arc <66992362+oxford-arc@users.noreply.github.com>
2020-07-02 12:24:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@ tar xzf hecbiosim-gromacs.tgz
 
 echo "Downloading Singularity image..."
 cd image
-singularity pull library://oxford-arc/default/gromacs2018.2-test
+singularity pull --name gromacs-2018.2.sif library://oxford-arc/default/gromacs2018.2-test:sha256.a62bf1b6c9a6e850441b13fd74ad70ed24e573c85d1600eb9bd48041cb9eec1d
+
 
 # If download above fails use the script below to build.
 # sh gromacs.sh
